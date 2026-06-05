ce8072a977cabc3dee03246fc231e6585278a669
OscarGO <oscar.gomez@utp.edu.co>
2026-06-05 18:03:50 -0500

    creando JPA entity intermedia en CursoUsuario

--- msvc-cursos/src/main/java/org/aguzman/springcloud/msvc/cursos/msvc/cursos/entity/Curso.java
+++ msvc-cursos/src/main/java/org/aguzman/springcloud/msvc/cursos/msvc/cursos/entity/Curso.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "courses")
 public class Curso {
@@ -14,6 +17,13 @@ public class Curso {
     @NotEmpty
     @Column(unique = true)
     private String nombre;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CursoUsuario> cursoUsuarios;
+
+    public Curso() {
+        cursoUsuarios = new ArrayList<>();
+    }
 
     public Long getId() {
         return id;
@@ -30,4 +40,21 @@ public class Curso {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    public void addCursoUsuario(CursoUsuario cursoUsuario){
+        cursoUsuarios.add(cursoUsuario);
+    }
+
+    public void removeCursoUsuario(CursoUsuario cursoUsuario){
+        cursoUsuarios.remove(cursoUsuario);
+    }
+
+    public List<CursoUsuario> getCursoUsuarios() {
+        return cursoUsuarios;
+    }
+
+    public void setCursoUsuarios(List<CursoUsuario> cursoUsuarios) {
+        this.cursoUsuarios = cursoUsuarios;
+    }
 }
+
---
+++ msvc-cursos/src/main/java/org/aguzman/springcloud/msvc/cursos/msvc/cursos/entity/CursoUsuario.java
@@ -0,0 +1,44 @@
+package org.aguzman.springcloud.msvc.cursos.msvc.cursos.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cursos_usuarios")
+
+public class CursoUsuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="usuario_id", unique = true)
+    private Long usuarioId;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Long getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
+        }
+        if (!(obj instanceof CursoUsuario)){
+            return false;
+        }
+        CursoUsuario o = (CursoUsuario) obj;
+        return this.usuarioId != null && this.usuarioId.equals(o.getUsuarioId());
+    }
+}
